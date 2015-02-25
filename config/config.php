5635d217f75480d381094b1b82078aa7116078da
zhuzhengyu <234079961@qq.com>
2015-02-25 22:41:14 +0800

    config commit

---
+++ config/config.php
@@ -0,0 +1,5 @@
+<?php
+include ('base.config.php');//基础配置配置
+include ('db.config.php');//数据库配置
+include ('path.config.php');//项目路径配置
+include ('url.config.php');//链接配置
---
+++ config/sub_config/db.config.php
@@ -0,0 +1,5 @@
+<?php
+define('DB_HOST', '');
+define('DB_PORT', '');
+define('DB_USER', '');
+define('DB_PSWD', '');
