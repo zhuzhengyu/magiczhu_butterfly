3e4e34a51be01e5df8f666dc4be76ad727dc1e10
aasdf <wer@adsf.com>
2015-03-02 18:43:03 +0800

    管理员管理

--- htdocs/admin/manage_admin.php
+++ htdocs/admin/manage_admin.php
@@ -6,27 +6,29 @@ include (MODEL_PATH . '/adminModel.php');//管理员类
 include (MODEL_PATH . '/moduleModel.php');//后台功能模块
 include (BASE_PATH . '/common/admin/common.php');//后台常用方法
 
+checkAdminLogin();//检查管理员登录状态
 
 include (ADMIN_VIEW_PATH . '/manage_admin.html');
+
 /**
  * @method 获取管理员列表
  */
 function admin_list() {
-	
+
 }
 
 function add_admin() {
-	
+
 }
 
 function commit_add_admin() {
-	
+
 }
 
 function edit_admin() {
-	
+
 }
 
 function commit_edit_admin() {
-	
+
 }
