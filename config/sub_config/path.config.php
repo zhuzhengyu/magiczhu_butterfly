c9a572ea84d6903ac5d1d4558dd3013fe20cff12
zhuzhengyu <234079961@qq.com>
2015-04-06 08:53:10 +0800

    批量提交

--- config/sub_config/path.config.php
+++ config/sub_config/path.config.php
@@ -1,6 +1,7 @@
 <?php
 define('BASE_PATH', 'D:/work/git/magiczhu_butterfly');//基础路径
 define('VIEW_PATH', BASE_PATH . '/htdocs/view');//前台模板路径
+define('FRONT_VIEW_PATH', BASE_PATH . '/htdocs/view/front');//前端页面路径
 define('ADMIN_VIEW_PATH', BASE_PATH . '/htdocs/view/admin');//后台模板路径
 define('MODEL_PATH', BASE_PATH . '/model');//数据库操作模块路径
 define('UPLOAD_PATH', BASE_PATH . '/htdocs/upload');
