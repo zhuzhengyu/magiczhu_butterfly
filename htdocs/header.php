c644b15fbb1df280de2217c53dcf0eef5eed58a2
zhuzhengyu <234079961@qq.com>
2015-04-06 08:52:42 +0800

    批量提交

---
+++ htdocs/header.php
@@ -0,0 +1,3 @@
+<?php
+// include ('../config/config.php');//基本配置
+include (FRONT_VIEW_PATH . '/header.html');
---
+++ htdocs/index.php
@@ -0,0 +1,4 @@
+<?php
+include ('../config/config.php');//基本配置
+include (FRONT_VIEW_PATH . '/index.html');
+?>
