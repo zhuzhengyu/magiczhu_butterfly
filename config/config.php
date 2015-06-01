d6dfc223940ccb6d7157ef12d366422c248fe112
zhuzhengyu <234079961@qq.com>
2015-06-01 20:04:06 +0800

    提交配置

--- config/config.php
+++ config/config.php
@@ -1,6 +1,7 @@
 <?php
-error_reporting(E_ALL^E_NOTICE);//@todo,生产环境关闭notice级别报错
+//error_reporting(E_ALL^E_NOTICE);//@todo,生产环境关闭notice级别报错
 include ('sub_config/base.config.php');//基础配置配置
 include ('sub_config/db.config.php');//数据库配置
 include ('sub_config/path.config.php');//项目路径配置
 include ('sub_config/url.config.php');//链接配置
+include ('sub_config/device.config.php');//设备配置
