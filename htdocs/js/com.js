f03cca6944782c39f0e5f2e6d139c1a0a9936b34
zhuzhengyu <234079961@qq.com>
2015-06-23 00:04:53 +0800

    增加JS

--- htdocs/js/com.js
+++ htdocs/js/com.js
@@ -22,13 +22,6 @@ $(function(){
 	});
 });
 
-$(function(){
-	$('.foryou-con ul li img').hover(function(){
-		$(this).addClass('flash animated');
-	},function(){
-		$(this).removeClass('flash animated');
-	});
-});
 
 $(function(){
 	$('.login a').hover(function(){
@@ -45,3 +38,65 @@ $(function(){
 		$(this).removeClass('flipInX animated');
 	});
 });
+
+$(function(){
+	$('.side-nav ul li a.i-nav').click(function(){
+		$('.side-nav ul li a').removeClass('on');
+		$(this).addClass('on');
+		if($(this).next("dl").is(':hidden')){
+			$(this).next("dl").slideDown();
+		}
+		else{
+			$(this).next("dl").slideUp();
+		}
+	});
+});
+
+$(function(){
+	$('.select_pro').click(function(){	
+		$('.select_pro').removeClass('select_on');
+		if($(this).hasClass('select_on')){
+			$(this).removeClass('select_on');
+		}
+		else{
+			$(this).addClass('select_on');
+		}
+	});
+});
+
+$(function(){
+	var WW = $('.recommend_pp_1').width();
+	var Now = 0;
+	var btn = 1;
+	$('.recommend_page a').click(function(){
+		var index = $(this).index();
+		$('.recommend_page a').removeClass('recommend_page_on')
+		$('.recommend_pp_con').stop().animate({'left': -WW*index+60},100);
+		$(this).addClass('recommend_page_on');
+		Now = index;
+	});
+	$('.pp_left').click(function(){
+		var m = $('.recommend_page a').size();
+		var left = parseInt($('.recommend_pp_con').css('left'));
+		if (left < 60 && Now>=0 && btn == 1) {
+			btn = 0;
+			$('.recommend_pp_con').stop().animate({'left': left+WW},100);
+			Now--;			
+		};
+		$('.recommend_page a').removeClass('recommend_page_on').eq(Now).addClass('recommend_page_on');
+		setTimeout(function () { btn = 1; }, 400);		
+	});
+	$('.pp_right').click(function(){		
+		var m = $('.recommend_page a').size();
+		var left = parseInt($('.recommend_pp_con').css('left'));
+		if (left > -WW*(m-1)+60 && Now <= m && btn == 1) {
+			btn = 0;
+			$('.recommend_pp_con').stop().animate({'left': left-WW},100);
+			Now++;			
+		}		
+		$('.recommend_page a').removeClass('recommend_page_on').eq(Now).addClass('recommend_page_on');
+		setTimeout(function () { btn = 1; }, 400);
+	});
+});
+
+
