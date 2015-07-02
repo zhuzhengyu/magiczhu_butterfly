e5c4c1c8dca2e538163cf73c6f3a60c25e61e4e4
zhuzhengyu <234079961@qq.com>
2015-07-02 23:56:06 +0800

    提交代码

--- htdocs/body.php
+++ htdocs/body.php
@@ -1,6 +1,6 @@
 <?php
-// include (FRONT_VIEW_PATH . '/body.html');
-$model = new indexPageModel();
-$detail = $model->get_detail_by_id(1);
-$body = base64_decode($detail['data']);
-echo $body;
+include (FRONT_VIEW_PATH . '/body.html');
+// $model = new indexPageModel();
+// $detail = $model->get_detail_by_id(1);
+// $body = base64_decode($detail['data']);
+// echo $body;
